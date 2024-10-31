ffe8e0a0a12aa015eda25cb6936e358a430c1c93
Matt <mphlamb@gmail.com>
2024-10-31 18:30:17 +0100

    character count rendered

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,3 +1,28 @@
-export default function Stats(): React.JSX.Element {
-	return <section className="stats">Stats</section>;
+type StatProp = {
+	label: string;
+	number: number;
+};
+
+function Stat({ label, number }: StatProp): React.JSX.Element {
+	return (
+		<section className="stat">
+			<span className="stat__number">{number}</span>
+			<h2 className="second-heading">{label}</h2>
+		</section>
+	);
+}
+
+export default function Stats({
+	numberOfCharacters,
+}: {
+	numberOfCharacters: number;
+}): React.JSX.Element {
+	return (
+		<section className="stats">
+			<Stat label={"Words"} number={0} />
+			<Stat label={"Characters"} number={numberOfCharacters} />
+			<Stat label={"Instagram"} number={0} />
+			<Stat label={"Facebook"} number={0} />
+		</section>
+	);
 }
