4f8d7e8cc32de6af362bb4905c1dad11ecb34a05
Matt <mphlamb@gmail.com>
2024-11-01 11:46:56 +0100

    imports updated

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
+import { FACEBOOK_MAX_CHARS, INSTAGRAM_MAX_CHARS } from "../constants";
 
 export default function Container(): React.JSX.Element {
 	const [text, setText] = useState<string>("");
 	const numberOfCharacters = text.length;
-	const instagramCharactersLeft: number = 280 - text.length;
-	const facebookCharactersLeft: number = 2200 - text.length;
+	const instagramCharactersLeft: number = INSTAGRAM_MAX_CHARS - text.length;
+	const facebookCharactersLeft: number = FACEBOOK_MAX_CHARS - text.length;
 	const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
 
 	return (
