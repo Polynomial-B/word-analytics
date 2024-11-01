7999bff629623d751c3384d30e78cd12cec708ca
Matt <mphlamb@gmail.com>
2024-11-01 11:38:43 +0100

    calcs updated

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,11 +5,19 @@ import Textarea from "./Textarea";
 export default function Container(): React.JSX.Element {
 	const [text, setText] = useState<string>("");
 	const numberOfCharacters = text.length;
+	const instagramCharactersLeft: number = 280 - text.length;
+	const facebookCharactersLeft: number = 2200 - text.length;
+	const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
 
 	return (
 		<main className="container">
 			<Textarea text={text} setText={setText} />
-			<Stats numberOfCharacters={numberOfCharacters} />
+			<Stats
+				numberOfCharacters={numberOfCharacters}
+				instagramCharactersLeft={instagramCharactersLeft}
+				facebookCharactersLeft={facebookCharactersLeft}
+				numberOfWords={numberOfWords}
+			/>
 		</main>
 	);
 }
